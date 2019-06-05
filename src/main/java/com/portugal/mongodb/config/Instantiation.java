e590055e689e0dc811b082a5c651ffabab07b622
alvaropotel <alvaropotel@gmail.com>
2019-06-05 22:49:26 +0100

    Add comments in the post

--- src/main/java/com/portugal/mongodb/config/Instantiation.java
+++ src/main/java/com/portugal/mongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.portugal.mongodb.domain.Post;
 import com.portugal.mongodb.domain.User;
 import com.portugal.mongodb.dto.AuthorDTO;
+import com.portugal.mongodb.dto.CommentDTO;
 import com.portugal.mongodb.repository.PostRepository;
 import com.portugal.mongodb.repository.UserRepository;
 
@@ -22,7 +23,6 @@ public class Instantiation implements CommandLineRunner {
 	
 	@Autowired
 	private PostRepository postRepository; 
-	
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -42,10 +42,20 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Portugal, here we go!", "I`m going to Lisbon!!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Good morning!", "Today is a big day!", new AuthorDTO(maria));
 
+		CommentDTO c1 = new CommentDTO("Have a safe trip", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Enjoy", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Have a good day", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
 		maria.getPost().addAll(Arrays.asList(post1,post2));
 		userRepository.save(maria);
+		
+		
+		
 		
 	}
 
