ab8619c5a923aff8925f83f133a371185aca2d15
alvaropotel <alvaropotel@gmail.com>
2019-06-08 16:02:50 +0100

    Complex @Querys - minDate and maxDate

--- src/main/java/com/portugal/mongodb/resources/PostResource.java
+++ src/main/java/com/portugal/mongodb/resources/PostResource.java
@@ -1,6 +1,8 @@
 package com.portugal.mongodb.resources;
 
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.portugal.mongodb.Services.PostService;
 import com.portugal.mongodb.domain.Post;
+import com.portugal.mongodb.domain.User;
+import com.portugal.mongodb.dto.UserDTO;
 import com.portugal.mongodb.resources.util.URL;
 
 @RestController
@@ -31,6 +35,19 @@ public class PostResource {
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text){
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	
+	@RequestMapping(value="/fullsearch", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> fullSearch(
+ 			@RequestParam(value="text", defaultValue="") String text,
+ 			@RequestParam(value="minDate", defaultValue="") String minDate,
+ 			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 	
